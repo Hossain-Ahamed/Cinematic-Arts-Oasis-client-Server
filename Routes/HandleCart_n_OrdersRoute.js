f37ad29fe0c93c1a21286c2b94f9be293f4c429f
Hossain Ahamed <hossainahamed6872@gmail.com>
2023-10-24 21:58:29 +0600

    purchase complete

--- Routes/HandleCart_n_OrdersRoute.js
+++ Routes/HandleCart_n_OrdersRoute.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const { verifyJWT } = require('../Middleware/middlewares');
-const { getCart, RemoveFromCart, addTocart, getDetailCart } = require('../controller/Cart_n_OrderController');
+const { getCart, RemoveFromCart, addTocart, getDetailCart, CreatePaymentIntent, detailOfTheItemOfIndividialItem, EnrollToClass } = require('../controller/Cart_n_OrderController');
 const router = express.Router();
 
 
 //get the cart
-router.get('/get-cart/:email',verifyJWT,getCart);
-router.get('/get-detail-cart/:email',verifyJWT,getDetailCart);
+router.get('/get-cart/:email', verifyJWT, getCart);
+
+
+//detailed with product
+router.get('/get-detail-cart/:email', verifyJWT, getDetailCart);
+
+//individual class detail data from cart
+router.get('/cart-data/detail/individucal/:RequsetedEmail/:cartItemID',verifyJWT,detailOfTheItemOfIndividialItem)
 
 //add to cart
-router.patch('/get-cart/add/:email',verifyJWT,addTocart);
+router.patch('/get-cart/add/:email', verifyJWT, addTocart);
 
 
 // delete from cart 
-router.patch('/get-cart/delete/:email',verifyJWT,RemoveFromCart);
+router.patch('/get-cart/delete/:email', verifyJWT, RemoveFromCart);
 
+//create payment intent
+router.post("/create-payment-intent", verifyJWT, CreatePaymentIntent);
+
+router.post("/enroll-in-class",verifyJWT,EnrollToClass);
 
 
 
--- controller/Cart_n_OrderController.js
+++ controller/Cart_n_OrderController.js
@@ -1,8 +1,10 @@
 const process = require('process');
-const { cartCollection, classCollection } = require('../Mongo/DataCollection');
+const { cartCollection, classCollection, enrolledStudentOfClassCollection } = require('../Mongo/DataCollection');
 const { ObjectId } = require('mongodb');
 
 
+// This is your test secret API key.
+const stripe = require("stripe")(process.env.PK_KEY);
 
 const getCart = async (req, res) => {
     try {
@@ -38,7 +40,7 @@ const getDetailCart = async (req, res) => {
                 const classDetail = await classCollection.findOne({ _id: new ObjectId(classId) });
                 return classDetail;
             }));
-     
+
 
             // Update the cart object to include the detailed item information
             const cartWithItemDetails = {
@@ -61,7 +63,7 @@ const getDetailCart = async (req, res) => {
 const addTocart = async (req, res) => {
     try {
         const { classID } = req.body;
-        console.log(req.body)
+        // console.log(req.body)
         if (req.data?.email !== req.params.email) {
             return res.status(401).send("unauthorized")
         }
@@ -121,9 +123,186 @@ const RemoveFromCart = async (req, res) => {
 
 }
 
+
+// individual detail of a cart 
+const detailOfTheItemOfIndividialItem = async (req, res) => {
+    try {
+
+        const { cartItemID, RequsetedEmail } = req.params;
+
+        if (req.data?.email !== RequsetedEmail) {
+            return res.status(401).send({ message: "unauthorized, email not same with token and request" })
+        }
+
+
+        //checking the requested cart id is in his cart data 
+        const existsInCart = await cartCollection.findOne(
+            {
+                email: RequsetedEmail,
+                items: { $in: [cartItemID] }
+            },
+            { projection: { _id: 1, items: 0, email: 0 } }// Fetch only the _id if the item exists
+        );
+
+        if (!existsInCart) {
+            return res.status(404).send({ message: "This is not found in your cart" })
+        }
+
+
+        const exist = await enrolledStudentOfClassCollection.findOne({ stdEmail: RequsetedEmail, class_ID: cartItemID });
+
+        if (exist) {
+            return res.status(409).send({ message: "You are already enrolled in this class" });
+        }
+
+
+        // if exists then fetch the data 
+        const classDetailByID = await classCollection.findOne(
+            {
+                _id: new ObjectId(cartItemID),
+                availableSeats: { $gt: 0 },
+                status: "Approved"
+            },
+            {
+                _id: 1,
+                email: 1,
+                name: 1,
+                UID: 1,
+                className: 1,
+                CoursePrice: 1,
+                availableSeats: 1
+            }
+        );
+
+        if (classDetailByID) {
+            // The document meets the criteria
+            res.status(200).send({ Details: classDetailByID, price: parseFloat(classDetailByID?.CoursePrice.toFixed(2)) });
+        } else {
+            // The document doesn't meet the criteria
+            res.status(404).send({ message: "Class not available to purchase" });
+        }
+
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({ message: "detailOfTheItemOfIndividialItem server error" })
+    }
+}
+
+
+// payment gateway 
+const CreatePaymentIntent = async (req, res) => {
+    try {
+
+
+        const { price } = req.body;
+
+        const ammount = parseInt(price * 100);
+
+
+        // Create a PaymentIntent with the order amount and currency
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: ammount,
+            currency: "usd",
+
+            // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+            automatic_payment_methods: {
+                enabled: true,
+            },
+        });
+
+        res.send({
+            clientSecret: paymentIntent.client_secret,
+        });
+    } catch (e) {
+        console.log(e);
+        // Bad Request: Server error or client sent an invalid request
+        res.status(500).send({ message: "Bad Request: Server error or invalid request" });
+    }
+};
+
+
+//enroll to new clss
+const EnrollToClass = async (req, res) => {
+    try {
+        const {
+            std_ID,
+            stdName,
+            stdEmail,
+            stdUID,
+            InstructorName,
+            InstructorEmail,
+            InstructorUID,
+            clssName,
+            class_ID,
+            transaction_method_email,
+            transaction_method_name,
+            transaction_method_phone,
+            transactionID,
+            intent_methodID,
+            methodID,
+            price,
+            Joindate,
+        } = req.body;
+
+        const exist = await enrolledStudentOfClassCollection.findOne({ stdEmail: stdEmail, class_ID: class_ID });
+        if (exist) {
+            return res.status(409).send({ message: "You are already enrolled in this class" });
+        }
+
+        const data = {
+            std_ID,
+            stdName,
+            stdEmail,
+            stdUID,
+            InstructorName,
+            InstructorEmail,
+            InstructorUID,
+            clssName,
+            class_ID,
+            transaction_method_email,
+            transaction_method_name,
+            transaction_method_phone,
+            transactionID,
+            intent_methodID,
+            methodID,
+            price,
+            Joindate,
+        };
+
+        const result = await enrolledStudentOfClassCollection.insertOne(data);
+
+
+
+        if (result.insertedId) {
+            // Document inserted successfully
+            await cartCollection.updateOne(
+                { email: stdEmail },
+                {
+                    $pull: { items: class_ID }
+                });
+
+            return res.status(200).send({ message: "Document inserted successfully" });
+        } else {
+            // Conflict: Insertion failed due to a client-related issue (e.g., duplicate data)
+            return res.status(400).send({ message: " Failed to insert the document" });
+        }
+    } catch (e) {
+        console.log(e);
+        // Bad Request: Server error or client sent an invalid request
+        res.status(500).send({ message: "Bad Request: Server error or invalid request" });
+    }
+};
+
+
 module.exports = {
     getCart,
     getDetailCart,
     addTocart,
-    RemoveFromCart
+    RemoveFromCart,
+    detailOfTheItemOfIndividialItem,
+
+
+    CreatePaymentIntent,
+    EnrollToClass
 }
